6c17f3a0bd200c755b5b4399c38f2d4e8ba2c96a
Abdullah Zafar <abdullah.zafar.career@gmail.com>
2024-08-03 08:36:59 +0500

    Sorted tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import "./App.css";
 
+const skills = [
+  {
+    name: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    name: "JavaScript",
+    level: "intermediate",
+    color: "#EFD81D",
+  },
+  {
+    name: "Web Design",
+    level: "beginner",
+    color: "#C3DCAF",
+  },
+  {
+    name: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    name: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+];
+
 export default function App() {
   return <Card />;
 }
@@ -14,22 +42,33 @@ const Card = () => {
 };
 
 const About = () => {
+  const sortedSkills = skills.sort((a, b) => {
+    const levels = { advanced: 1, intermediate: 2, beginner: 3 };
+    return levels[a.level] - levels[b.level];
+  });
+
   return (
     <article className="p-5 text-left">
       <header>
         <h1 className="text-3xl sm:text-4xl font-bold ">Abdullah Zafar</h1>
       </header>
       <p className="mt-6 text-sm lg:text-base md:text-2xl sm:text-xl font-thin">
-        A passionate <span>Full-stack web developer</span> from Pakistan.
-        Programming being my passion, I spend a lot of my time infront of
-        computer system turning my ideas into reality.
+        A passionate{" "}
+        <span className="bg-yellow-300 hover:bg-yellow-400 px-1">
+          Full-stack web developer
+        </span>{" "}
+        from Pakistan. Programming being my passion, I spend a lot of my time
+        infront of computer system turning my ideas into reality.
       </p>
-      <section className="w-80 mt-5">
-        <TechStack name="React" color="lightblue" />
-        <TechStack name="NodeJS" color="green" />
-        <TechStack name="JavaScript" color="yellow" />
-        <TechStack name="HTML/CSS" color="red" />
-        <TechStack name="Git" color="orange" />
+      <section className="w-96 mt-5">
+        {sortedSkills.map((skill) => (
+          <TechStack
+            key={skill.name}
+            skill={skill.name}
+            color={skill.color}
+            level={skill.level}
+          />
+        ))}
       </section>
     </article>
   );
@@ -39,11 +78,14 @@ const TechStack = (props) => {
   return (
     <div className=" inline-block p-3">
       <span
-        className="border-2 border-slate-950 py-1 px-2 rounded-lg"
+        className="border-2 border-slate-950 py-1 px-4 rounded-lg font-bold"
         style={{ backgroundColor: props.color }}
       >
-        {props.name}
+        {props.skill} {props.level === "advanced" && "III"}
+        {props.level === "intermediate" && "II"}
+        {props.level === "beginner" && "I"}
       </span>
+      <span></span>
     </div>
   );
 };
